b6ea34a4e27582de10aed0584c6a7447301fef0b
yogaaltariz <yoga.altariz@gmail.com>
2022-07-05 17:52:02 +0700

    new hero home page

---
+++ assets/js/twb.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function () {
+    const scroll = window.scrollY
+
+    if (scroll > 80) {
+        document.querySelector('.header__twb').classList.add('scrolled')
+        document.querySelector('.header__brand').setAttribute('src', 'assets/img/logos/Logo-twb.svg')
+        document.querySelector('.header__brand-mobile').setAttribute('src', 'assets/img/logos/logo-twb-icon.svg')
+
+    } else {
+        document.querySelector('.header__brand').setAttribute('src', 'assets/img/logos/Logo-twb-white.svg')
+        document.querySelector('.header__brand-mobile').setAttribute('src', 'assets/img/logos/logo-twb-icon-white.svg')
+        document.querySelector('.header__twb').classList.remove('scrolled')
+    }
+    // console.log(scroll)
+})
